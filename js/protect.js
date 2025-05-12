6454d01ab590d07d860b3d9f6ae128651d2fab71
mukesh-f2p <mukesh-f2p@users.noreply.github.com>
2025-05-12 02:48:03 +0000

    deploy: 5fdf37cedacd2a2b406c3853abecb76db9184736

--- js/protect.js
+++ js/protect.js
@@ -5,5 +5,6 @@ const fullBaseUrl = window.location.origin + "/" + basePath;
 // Check if user is authenticated
 if (localStorage.getItem('authenticated') !== 'true') {
     // Redirect to login page using the correct base path
+    console.log(`${window.location.origin}/login.html`);
     window.location.href = `${window.location.origin}/login.html`;  // Redirect to login if not authenticated
 }
